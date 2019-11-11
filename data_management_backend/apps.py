76a05d3f5a7f8bff825c6ab7fa133ae547798312
josias <kologojosias@gmail.com>
2019-11-11 19:05:10 +0100

    Undone

--- data_management_backend/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataManagementBackendConfig(AppConfig):
-    name = 'data_management_backend'
--- data_management_frontend/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataManagementFrontendConfig(AppConfig):
-    name = 'data_management_frontend'
