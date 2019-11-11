d96def3f1ec043c66661147d5f2784576b394d2c
josias <kologojosias@gmail.com>
2019-11-11 19:09:31 +0100

    Code

---
+++ data_management_backend/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+import secrets
+
+# Create your models here.
+
+class File(models.Model):
+	title = models.CharField(max_length=100, default='default title')
+	file = models.FileField(upload_to='files/')
+
+	def __str__(self):
+		return self.title
+
+
+
+class Token(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tokens')
+	string = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.string
+
+	def generateToken(self):
+		self.string = secrets.token_urlsafe(16)
+
+
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				}
+			},
+			{
+				test: /\.css$/,
+				use: {
+					loader: 'css-loader',
+				}
+			}
+
+		]
+	},
+	watch: true,
+}
