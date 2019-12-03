b2e9b952eb8d9a5c891c580ea71bfc74cafe8b51
Josias <kologojosias@gmail.com>
2019-12-03 09:28:04 +0100

    host added

--- data_management/settings.py
+++ data_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jq3qq8!wkeh=-b!#0a=tk#ks-nnqvfvh=@3z=lhxrw6z=6k&##'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'datavisualise.herokuapp.com']
 
 
 # Application definition
