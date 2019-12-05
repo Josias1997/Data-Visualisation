34fdb9923f4737589884742b5e94ef10a64dc1b3
Josias <kologojosias@gmail.com>
2019-12-05 13:11:29 +0100

    database added

--- data_management/settings.py
+++ data_management/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'data_management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'NAME': 'vcb0VQtd1L',
+        'USER': 'vcb0VQtd1L',
+        'PASSWORD': 'BGbNOkog7h',
+        'PORT': '3306',
+        'HOST': 'remotemysql.com',
     }
 }
-# 'NAME': 'vcb0VQtd1L',
-# 'USER': 'vcb0VQtd1L',
-# 'PASSWORD': 'BGbNOkog7h',
-# 'PORT': '3306',
-# 'HOST': 'remotemysql.com',
+
 
 
 # Password validation
