99db9e42ee42cd8deaf92512c2d4f20ef8b80ac6
Josias <kologojosias@gmail.com>
2019-12-10 12:41:16 +0100

    Bug fixed

--- data_management_frontend/src/components/Routes/Routes.js
+++ data_management_frontend/src/components/Routes/Routes.js
@@ -4,15 +4,18 @@ import DataManipulationPage from '../Pages/DataManipulation/DataManipulationPage
 import RStatisticsPage from '../Pages/RStatistics/RStatisticsPage';
 import ModelisationPage from '../Pages/Modelisation/ModelisationPage';
 import MachineLearning from '../Pages/MachineLearning/MachineLearning';
+import ErrorBoundary from "../HOC/ErrorBoundary/ErrorBoundary";
 
 const Routes = props => {
     return (
-        	<Switch>
+    	<ErrorBoundary>
+    		 <Switch>
 	            <Route exact path={"/"} component={DataManipulationPage} />
 	            <Route path={"/r-statistics"} component={RStatisticsPage}/>
 	            <Route path={"/modelisation"} component={ModelisationPage} />
 	            <Route path={"/machine-learning"} component={MachineLearning} />
        	 	</Switch>
+    	</ErrorBoundary>
     )
 }
 export default Routes;
